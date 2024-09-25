5bce2433629d9f24a8064e787088ce7489004aeb
Tiago Lima <51231966+tiagoadv7@users.noreply.github.com>
2024-09-25 11:26:58 -0300

    Ajustes e melhorias

--- main.js
+++ main.js
@@ -1,22 +1,49 @@
 const { app, BrowserWindow, ipcMain, dialog, screen } = require('electron');
 const path = require('path');
-let mainWindow, videoWindow;
+let mainWindow, videoWindow, loadingWindow;
 
 app.on('ready', () => {
-  // Criar a janela principal
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  // Criar a janela de carregamento
+  loadingWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    frame: false,
+    transparent: true,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       contextIsolation: false
-    },
-    icon: path.join(__dirname, 'assets/icons/icon.png')
+    }
   });
 
-  mainWindow.setMenu(null);  // Remove o menu padrão do Electron
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  loadingWindow.loadFile(path.join(__dirname, 'loading.html')); // Carrega a tela de loading
+
+  // Após criar a janela de carregamento, criamos a janela principal
+  setTimeout(createMainWindow, 2000); // Delay de 2 segundos para simular o carregamento
+
+  function createMainWindow() {
+    // Criar a janela principal
+    mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: true,
+        contextIsolation: false
+      },
+      icon: path.join(__dirname, 'assets/icons/icon.png')
+    });
+
+    mainWindow.setMenu(null);  // Remove o menu padrão do Electron
+    mainWindow.loadFile(path.join(__dirname, 'index.html')); // Carrega a página principal
+
+    // Quando a página principal estiver pronta, fechamos a janela de carregamento
+    mainWindow.webContents.once('dom-ready', () => {
+      if (loadingWindow) {
+        loadingWindow.close(); // Fecha a janela de loading
+      }
+      mainWindow.show(); // Exibe a janela principal
+    });
+  }
 
   // Abrir o diálogo de seleção de arquivos
   ipcMain.on('open-file-dialog', async (event) => {
